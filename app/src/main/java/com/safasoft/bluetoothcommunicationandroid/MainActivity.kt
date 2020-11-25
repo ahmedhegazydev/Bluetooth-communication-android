59ab60276bac7b35c53a47edc6a29b3b2477b8fd
Ahmed Hegazi <engahmedali2022@gmail.com>
2020-11-24 21:04:39 -0800

    v

--- app/src/main/java/com/safasoft/bluetoothcommunicationandroid/MainActivity.kt
+++ app/src/main/java/com/safasoft/bluetoothcommunicationandroid/MainActivity.kt
@@ -1,25 +1,44 @@
 package com.safasoft.bluetoothcommunicationandroid
 
+import android.R
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
 import android.os.Bundle
+import android.util.Log
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainActivity : AppCompatActivity() {
 
+    val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+    private var listView: ListView? = null
+    private val mDeviceList = ArrayList<String>()
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        listView = findViewById(R.id.listView)
+
+
+        mBluetoothAdapter.startDiscovery();
+        val filter = IntentFilter(BluetoothDevice.ACTION_FOUND)
+        registerReceiver(mReceiver, filter)
+
     }
 
 
     override fun onPostCreate(savedInstanceState: Bundle?) {
         super.onPostCreate(savedInstanceState)
 
-        val mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (mBluetoothAdapter == null) {
             // Device does not support Bluetooth
 
@@ -45,11 +64,48 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_LONG).show()
 //                bluetoothEnabled()
 
+//                val pairedDevices = mBluetoothAdapter.bondedDevices
+//                val s: MutableList<String> = ArrayList()
+//                for (bt in pairedDevices) s.add(bt.name)
+//                setListAdapter(ArrayAdapter(this, R.layout.list, s))
+
+
             } else if (resultCode == RESULT_CANCELED) {
                 Toast.makeText(this, "User canceled", Toast.LENGTH_LONG).show()
             }
         }
     }
 
+
+    override fun onDestroy() {
+        unregisterReceiver(mReceiver)
+        super.onDestroy()
+    }
+
+    private val mReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent
+                    .getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                mDeviceList.add(
+                    """
+                    ${device!!.name}
+                    ${device.address}
+                    """.trimIndent()
+                )
+                Log.i(
+                    "BT", """
+     ${device.name}
+     ${device.address}
+     """.trimIndent()
+                )
+                listView?.adapter = ArrayAdapter<String>(
+                    this@MainActivity,
+                    R.layout.simple_list_item_1, mDeviceList
+                )
+            }
+        }
+    }
 
 }
